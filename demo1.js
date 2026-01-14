eb3d9176a740209856c455ecfcb187882c093d25
HackerOm <hackeromsutar@gmail.com>
2026-01-14 09:03:12 +0530

    perform First Activity which demonstrate Types of DataTypes in js

---
+++ demo1.js
@@ -0,0 +1,36 @@
+// This is About Types of datatypes in javascript 
+
+
+//                  Primitive DataType          
+// 1)number
+// 2)String
+// 3)bool
+// 4)Undefined
+// 5)none
+// 6)Symbolic
+// 7)BigInt
+
+//              NON Primitive DataType          
+
+// 1)Ojbect
+
+
+let a = 12  // Number
+let b = "Hackers"  // String
+let c = true  // Boolean
+let d = undefined  // undefined
+let e = null  // none
+let f = 123n  // BigInt
+
+
+let obj = {1 :"Hacker",
+    2:"om"
+}
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(d);
+console.log(e);
+console.log(f);
+console.log(obj);
+
