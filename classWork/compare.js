b125f0b94d24bc01269466e91116d4342b9a81b7
HackerOm <hackeromsutar@gmail.com>
2026-01-15 21:21:12 +0530

    Some modification in folder stucture and solved Activity in demo3 file

---
+++ classWork/compare.js
@@ -0,0 +1,34 @@
+
+//comparison of two numbers
+console.log(2>1);
+console.log(2<1);
+console.log(2>=1);
+console.log(2<=1);
+console.log(2==1);
+console.log(2!=1);
+console.log();
+console.log();
+
+//comparison of string and number 
+console.log("2">1);
+console.log("02">2);
+console.log();
+console.log();
+
+
+//comparison of null with 0 
+console.log(null>0);
+console.log(null==0);
+console.log(null<=0);
+console.log();
+console.log();
+
+//comparison of Undefined with 0
+console.log(undefined>0);
+console.log(undefined==0);
+console.log(undefined<=0);
+console.log();
+console.log();
+
+// comparison with === equals to check datatype also 
+console.log("2" === 2);
---
+++ classWork/demo3.js
@@ -0,0 +1,115 @@
+
+
+
+let a = 12  // Number
+let b = "Hackers"  // String
+let c = true  // Boolean
+let d = undefined  // undefined
+let e = null  // none
+let f = 123n
+
+// Activity 1 converstion 
+
+//Value to Strings 
+let aToStr = String(a);
+console.log("String value a : "+aToStr);
+let bToStr = String(b);
+console.log("String value b : "+bToStr);
+let cToStr = String(c);
+console.log("String value c : "+cToStr);
+let dToStr = String(d);
+console.log("String value d : "+dToStr);
+let eToStr = String(e);
+console.log("String value e : "+eToStr);
+let fToStr = String(f);
+console.log("String value f : "+fToStr);
+console.log();
+console.log();
+
+
+// Values to Number
+let aToNum = Number(a);
+console.log("Number value a : "+aToNum);
+let bToNum = Number(b);
+console.log("Number value b : "+bToNum);
+let cToNum = Number(c);
+console.log("Number value c : "+cToNum);
+let dToNum = Number(d);
+console.log("Number value d : "+dToNum);
+let eToNum = Number(e);
+console.log("Number value e : "+eToNum);
+let fToNum = Number(f);
+console.log("Number value f : "+fToNum);
+console.log();
+console.log();
+
+
+//Values to Boolean
+let aToBool = Boolean(1);
+console.log("Boolean  value 1 : "+aToBool);
+let bToBool = Boolean(0);
+console.log("Boolean  value 0 : "+bToBool);
+console.log();
+console.log();
+//Value to BigInt
+
+let aToBig = BigInt(a);
+console.log("Value of A in BigInt :"+aToBig);
+console.log();
+console.log();
+
+
+
+// ************Operations *************************
+
+console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2%2);
+console.log(2/2);
+
+
+let str1 = "hello";
+let str2 = "Hacker";
+
+
+
+
+// Imp note :- javascirpt only automaticaly convert a string to number in arithmetic operations
+
+//concatination of String and Numbers 
+console.log(str1+" "+str2);
+console.log("1"+2);
+console.log("1"+2+2);
+console.log(1+2+"2");
+console.log();
+console.log();
+
+//Operation on Boolean data 
+
+console.log(true);
+console.log(+true);
+console.log(true+" ");
+console.log();
+console.log();
+
+
+// Pre and Post increments 
+let gamecounter = 100;
+gamecounter++;
+console.log(gamecounter);
+console.log();
+console.log();
+
+
+let scorecounter = 200;
+++scorecounter;
+console.log(scorecounter);
+console.log();
+console.log();
+
+
+
+
+
+
